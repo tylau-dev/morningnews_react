6f61d9767c395874f5188ba941ae3556ce0bd432
TY Lau <lautsyyeung@gmail.com>
2021-02-12 12:02:09 +0100

    border lang + lang in wishlisht schema

--- models/wishlists.js
+++ models/wishlists.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 const wishlistsSchema = mongoose.Schema({
-    articles : [ { title: String, description: String, content: String, image: String}]
+    articles : [ { title: String, description: String, content: String, image: String, language: String}]
 })
 
 const wishlistsModel = mongoose.model('wishlists', wishlistsSchema)
