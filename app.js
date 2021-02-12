1b40c412a57b2841801af4bf463257622f478bb7
TY Lau <lautsyyeung@gmail.com>
2021-02-12 17:05:57 +0100

    app.js change

--- app.js
+++ app.js
@@ -20,13 +20,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 //app.use(express.static(path.join(__dirname, 'public')));
-// app.use(express.static(path.join(__dirname, 'reactapp/build')));
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.get('*', function(req, res) {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
-
+app.use(express.static(path.join(__dirname, 'reactapp/build')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
